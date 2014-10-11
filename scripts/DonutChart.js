014f752d6e0c097cdf72947ead68ef1260277190
Mahesh <mahesh.h4evr@gmail.com>
2014-10-11 21:17:10 +0530

    Tab indent correction

--- scripts/DonutChart.js
+++ scripts/DonutChart.js
@@ -3,30 +3,30 @@
 var d3 = require('d3');
 
 module.exports = {
-	render: function (selector, data) {
-		'use strict';
-		var selection = d3.select(selector);
+  render: function (selector, data) {
+    'use strict';
+    var selection = d3.select(selector);
 
-		if (!selection) {
-			throw new Error("No elements for selector:" + selector);
-		}
+    if (!selection) {
+      throw new Error("No elements for selector:" + selector);
+    }
 
-		var arc = d3.svg.arc()
-    	.innerRadius(20)
-    	.outerRadius(40)
-    	.startAngle(0);
+    var arc = d3.svg.arc()
+      .innerRadius(20)
+      .outerRadius(40)
+      .startAngle(0);
 
     var g = selection.append("g")
-    	.attr("transform", "translate(50, 50)");
+      .attr("transform", "translate(50, 50)");
 
     var background = g.append("path")
-	    .datum({endAngle: 2 * Math.PI})
-	    .style("fill", "#7f8c8d")
-	    .attr("d", arc);
+      .datum({endAngle: 2 * Math.PI})
+      .style("fill", "#7f8c8d")
+      .attr("d", arc);
 
-	  var foreground = g.append("path")
-	    .datum({endAngle: .4 * 2 * Math.PI})
-	    .style("fill", "#3498db")
-	    .attr("d", arc);
-	}
+    var foreground = g.append("path")
+      .datum({endAngle: .4 * 2 * Math.PI})
+      .style("fill", "#3498db")
+      .attr("d", arc);
+  }
 };
